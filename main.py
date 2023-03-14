aa29176b3006002c73e0236554d717096adc69b3
Łukasz Kuk <lukasz010420@gmail.com>
2023-03-15 00:43:07 +0100

    footer:

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fpdf import FPDF
 import pandas as pd
 
+def set_footer():
+    pdf.set_font(family="Times", style="I", size=8)
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=12, txt=row["Topic"], align="R")
+
 if __name__ == "__main__":
     pdf = FPDF(orientation="P", unit="mm", format="A4")
-
+    pdf.set_auto_page_break(auto=False, margin=0)
     df = pd.read_csv("topics.csv")
 
     for index, row in df.iterrows():
@@ -13,5 +18,16 @@ if __name__ == "__main__":
         pdf.set_text_color(100, 100, 100)
         pdf.cell(w=0, h=12, txt=row["Topic"], align="L", ln=1)
         pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+        # Set the footer
+        pdf.ln(265)
+        set_footer()
+
+        for i in range(row["Pages"] - 1):
+            pdf.add_page()
+
+            # Set the footer
+            pdf.ln(277)
+            set_footer()
 
     pdf.output("output.pdf")
